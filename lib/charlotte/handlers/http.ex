077e73114cde98ce4091967de557adfd5558f66c
Glen Holcomb <leakybucket@Glens-MacBook-Pro.local>
2014-08-11 19:56:13 -0600

    Whitespace.

--- lib/charlotte/handlers/http.ex
+++ lib/charlotte/handlers/http.ex
@@ -1,8 +1,8 @@
 defmodule Charlotte.Handlers.HTTP do
   @moduledoc """
-    The HTTP Handler module provides the basic HTTP 1.1 callbacks Cowboy expects in a route handler.  
+    The HTTP Handler module provides the basic HTTP 1.1 callbacks Cowboy expects in a route handler.
 
-    This is important because each controller in Charlotte is treated as a Reuqest Handler for the routes it defines.  
+    This is important because each controller in Charlotte is treated as a Reuqest Handler for the routes it defines.
 
     To build an HTTP 1.1 Controller simply require this module and call the setup macro.  Your Controller Module
     should implement a routes method which returns a list of {path, action} tuples.
@@ -10,16 +10,16 @@ defmodule Charlotte.Handlers.HTTP do
 
   @doc """
     Using sets up the definitions for the default init, handle and terminate callbacks.  These callbacks assume
-    the controller module API follows what is outlined in the documentation.  
+    the controller module API follows what is outlined in the documentation.
 
     The handle callback is the most complex behavior wise.  It builds a Charlotte.Req.Conn Record, it looks
-    up the action for the path and calls the action with the verb, params, and conn record.  
+    up the action for the path and calls the action with the verb, params, and conn record.
 
-    setup also adds three convenience functions for crafting responses for the client:  
+    setup also adds three convenience functions for crafting responses for the client:
 
-    * render(status \\ 200, bindings, conn)  
-    * redirect(status \\ 302, conn)  
-    * forbidden(conn)  
+    * render(status \\ 200, bindings, conn)
+    * redirect(status \\ 302, conn)
+    * forbidden(conn)
   """
   defmacro __using__(_args) do
     quote do
