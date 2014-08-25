cbffbd215bd78caabb1c115cc23f3b99803278f6
Glen Holcomb <DamnBigMan@gmail.com>
2014-08-25 16:14:55 -0600

    Cleanup `Charlotte.Handlers.HTTP` tests.

--- test/charlotte/handlers/http_test.exs
+++ test/charlotte/handlers/http_test.exs
@@ -4,33 +4,23 @@ defmodule FakeController do
   def routes do
     [{"/bob", :bob}]
   end
-
-  def bob(verb, params, conn) do
-    render [], conn
-  end
 end
 
 defmodule Charlotte.Handlers.HTTPTest do
   use ExUnit.Case
   use ExSpec
 
-  describe "setup" do
+  describe "__using__" do
     it "Adds the init callback for Cowboy" do
-      assert FakeController.init({:tcp, :http}, [], []) == {:ok, [], []}
+      assert function_exported?(FakeController, :init, 3)
     end
 
-    # it "Adds the handle callback for Cowboy" do
-    #   FakeController.handle([], []) |> equals :ok
-    # end
+    it "Adds the handle callback for Cowboy" do
+      assert function_exported?(FakeController, :handle, 2)
+    end
 
     it "Adds the terminate callback for Cowboy" do
-      assert FakeController.terminate([], [], []) == :ok
+      assert function_exported?(FakeController, :terminate, 3)
     end
-
-    # it "Adds the render function" do
-    #   provided [FakeController.render(_, _, _) |> :ok, FakeController.bob(_, _, _) |> FakeController.render] do
-    #     FakeController.bob("GET", [param: "param"], []) |> equals :ok
-    #   end
-    # end
   end
 end
